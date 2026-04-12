d32b062927dc7d1eb26b663adfa7244c42ecb151
Baida98 <144565569+Baida98@users.noreply.github.com>
2026-04-12 14:52:43 +0200

    Update analytics.js

--- engine/analytics.js
+++ engine/analytics.js
@@ -1,32 +1,17 @@
-import { db } from "../firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+export function stats(history) {
 
-export async function buildStats() {
+  const total = history.length;
 
-  const snap = await getDocs(collection(db,"preventivi"));
+  const avg = history.reduce((s, x) => s + x.mid, 0) / (total || 1);
 
-  const map = {};
+  const byType = history.reduce((acc, x) => {
+    acc[x.tipo] = (acc[x.tipo] || 0) + 1;
+    return acc;
+  }, {});
 
-  snap.forEach(d => {
-    const x = d.data();
-
-    if(!map[x.tipo]) map[x.tipo] = [];
-
-    map[x.tipo].push(Number(x.prezzo));
-  });
-
-  const stats = {};
-
-  for(let k in map){
-    const arr = map[k];
-
-    const avg = arr.reduce((a,b)=>a+b,0)/arr.length;
-
-    stats[k] = {
-      avg,
-      count: arr.length
-    };
-  }
-
-  return stats;
+  return {
+    total,
+    avg,
+    byType
+  };
 }
