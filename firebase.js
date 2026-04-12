cf757093dcf994dc2205a80d27bd07c3a6702969
Baida98 <144565569+Baida98@users.noreply.github.com>
2026-04-12 13:37:15 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,30 +1,25 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
   getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
-  onAuthStateChanged
+  GoogleAuthProvider
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  query,
-  where,
-  getDocs
+  getFirestore
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "INSERISCI",
-  authDomain: "INSERISCI.firebaseapp.com",
-  projectId: "INSERISCI",
-  appId: "INSERISCI"
+  apiKey: "AIzaSyBJ8jCMTDRvvKHQ3ezwlhOcyQK3tTpDQfo",
+  authDomain: "preventivi-smart.firebaseapp.com",
+  databaseURL: "https://preventivi-smart-default-rtdb.firebaseio.com",
+  projectId: "preventivi-smart",
+  storageBucket: "preventivi-smart.firebasestorage.app",
+  messagingSenderId: "292306670153",
+  appId: "1:292306670153:web:42623bf882f3b98c1a2598"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
