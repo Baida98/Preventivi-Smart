f15fa1ca3d546a55adc24731b30429372540726e
Baida98 <144565569+Baida98@users.noreply.github.com>
2026-04-12 13:33:31 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,28 +1,30 @@
-// Firebase SDK (Web App)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-analytics.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// CONFIG FIREBASE
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBJ8jCMTDRvvKHQ3ezwlhOcyQK3tTpDQfo",
-  authDomain: "preventivi-smart.firebaseapp.com",
-  databaseURL: "https://preventivi-smart-default-rtdb.firebaseio.com",
-  projectId: "preventivi-smart",
-  storageBucket: "preventivi-smart.firebasestorage.app",
-  messagingSenderId: "292306670153",
-  appId: "1:292306670153:web:42623bf882f3b98c1a2598",
-  measurementId: "G-MFM7NPFNZ5"
+  apiKey: "INSERISCI",
+  authDomain: "INSERISCI.firebaseapp.com",
+  projectId: "INSERISCI",
+  appId: "INSERISCI"
 };
 
-// INIT APP
 const app = initializeApp(firebaseConfig);
 
-// SERVICES
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-const rtdb = getDatabase(app);
-
-// EXPORT (per usare nei tuoi file)
-export { app, analytics, db, rtdb };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
